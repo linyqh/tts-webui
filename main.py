fc184c1eccad4398b1f2694eaf4d4b75fac3695d
linyq <linyqemail@163.com>
2024-08-23 12:18:16 +0800

    调整目录结构；修改url有效判断

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 pg = st.navigation([
-    st.Page("coqui.py", title="Coqui TTS", icon="🐸"),
-    st.Page("openvoice.py", title="OpenVoice TTS", icon="🔥"),
-    st.Page("recording.py", title="录音机", icon="🎙️")
+    st.Page("apps/coqui.py", title="Coqui TTS", icon="🐸"),
+    st.Page("apps/openvoice.py", title="OpenVoice TTS", icon="🔥"),
+    st.Page("apps/recording.py", title="录音机", icon="🎙️")
 ])
 pg.run()
--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ import requests
 # 判断是否为有效的url
 def is_valid_url(url):
     try:
-        response = requests.head(url)
-        return response.status_code == 200 or response.status_code == 404
+        response = requests.head(url + '/docs')
+        return response.status_code == 200
     except:
         return False
